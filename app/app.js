9b64fe605d695db5d1f389447d87d42dd22bbd33
alex <uohzxela@gmail.com>
2015-06-14 12:55:37 +0800

    add nav bar, usage instructions, and clean up homepage

--- app/app.js
+++ app/app.js
@@ -6,7 +6,21 @@ angular.module('myApp', [
   'myApp.home',
   'ngFileUpload',
   'angular-loading-bar'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/home'});
+])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+  .when('/dos-n-donts', {
+    templateUrl: 'misc/dos-n-donts.html',
+  })
+  .when('/how-it-works', {
+  	templateUrl: 'misc/how-it-works.html'
+  })
+  .otherwise({redirectTo: '/home'});
+}])
+
+.controller('HeaderController', ['$scope', '$location', function($scope, $location) {
+    $scope.isActive = function (viewLocation) { 
+        return viewLocation === $location.path();
+    };
 }]);
